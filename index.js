c76ac99101ce1269846ac24f48e7749f708626fd
mkoenke <mkoenke@sas.upenn.edu>
2020-11-18 19:29:12 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+function findMatching(array, string){
+    return array.filter( e => 
+        e.toLowerCase() === string.toLowerCase()
+    )
+}
+
+// function findMatching(source, sought) {
+//     return source.filter( possibleMatch =>
+//       possibleMatch.toLowerCase() === sought.toLowerCase()
+//     )
+//   }
+
+function fuzzyMatch(drivers, string){
+    return drivers.filter( e =>
+        e.toLowerCase().indexOf(string.toLowerCase()) === 0
+
+    )
+}
+
+function matchName(drivers, string){
+    return drivers.filter(ele =>
+        ele.name === string
+        )
+}
